f28413e645d56a1aa062393be95c7b06a6b09e65
Frederico Carvalho <frederico.ferreira.carvalho@gmail.com>
2025-03-14 21:37:06 +0000

    Special Done

--- Special/Program.cs
+++ Special/Program.cs
@@ -4,9 +4,23 @@ namespace Special
 {
     public class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello LP1!");
+            Console.Write("Insert n-th number of the special sequence: ");
+            if (int.TryParse(Console.ReadLine(), out int n) && n >= 0)
+            {
+                Console.WriteLine(GetSpecial(n));
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a non-negative integer.");
+            }
+        }
+        private static int GetSpecial(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            return GetSpecial(n - 1) + 2 * GetSpecial(n - 2);
         }
     }
 }
