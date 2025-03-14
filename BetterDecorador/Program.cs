b2f856d3518df5cb79570105b55f3aad041c414f
Frederico Carvalho <frederico.ferreira.carvalho@gmail.com>
2025-03-14 20:24:50 +0000

    Comentários XML Adicionados

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿﻿using System;
 
 namespace BetterDecorador
-
 {
+    /// <summary>
+    /// O programa serve para decorar uma string dada cercando-a com um caracter repetido.
+    /// Se nao forem dados nenhuns argumentos, será dado uma mensagem padrão
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Inicio do program, onde se analisa argumentos de linha, de comando e chama o método Decor apropriado.
+        /// </summary>
+        /// <param name="args">
+        /// Um array de strings contém:
+        /// args[0] - O texto a ser decorado.
+        /// args[1] - Um unico caracter a ser usado para o decorator.
+        /// args[2] - Um integer especificando quantas vezes o caracter decorator deveria repetir.
+        /// Se nao forem dados nenhuns argumentos, será dado uma mensagem padrão
+        /// </param>
         private static void Main(string[] args)
         {
             if (args.Length < 3)
@@ -30,12 +43,23 @@ namespace BetterDecorador
             Console.WriteLine(Decor(text, decorator, repeat));
         }
 
+        /// <summary>
+        /// Decora uma determinada string circundando-a com um caractere repetido.
+        /// </summary>
+        /// <param name="s">O texto onde se aplicara o decorated.</param>
+        /// <param name="dec">O caracter usado para o decoration.</param>
+        /// <param name="count"> Número de vezes em que o character com a decoration deveria repetir.</param>
+        /// <returns>Uma string formatada com a decoration aplicada nela. </returns>
         private static string Decor(string s, char dec, int count)
         {
             string decoration = new string(dec, count);
             return $"{decoration} {s} {decoration}";
         }
 
+        /// <summary>
+        /// Retorna uma decorated message normal quando não são apresentados argumentos. 
+        /// </summary>
+        /// <returns>Uma string padrão formatada</returns>
         private static string Decor()
         {
             return Decor("User did not specify args!", '=', 3);
