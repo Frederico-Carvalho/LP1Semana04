bfd9fdef183283b7a7ad99481bcae2e5acf70f23
Frederico Carvalho <frederico.ferreira.carvalho@gmail.com>
2025-03-14 19:56:57 +0000

    BetterDecorador Done

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 
 namespace BetterDecorador
+
 {
     public class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            if (args.Length < 3)
+            {
+                Console.WriteLine(Decor());
+                return;
+            }
+
+            string text = args[0];
+            char decorator;
+            int repeat;
+
+            if (!char.TryParse(args[1], out decorator))
+            {
+                return;
+            }
+
+            if (!int.TryParse(args[2], out repeat) || repeat < 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(Decor(text, decorator, repeat));
+        }
+
+        private static string Decor(string s, char dec, int count)
+        {
+            string decoration = new string(dec, count);
+            return $"{decoration} {s} {decoration}";
+        }
+
+        private static string Decor()
+        {
+            return Decor("User did not specify args!", '=', 3);
         }
     }
 }
